ad3a2af64acec7c8dfb6a4289aa79fc8b971af93
yunashrestha2024-create <yunashrestha2024@gmail.com>
2026-08-22 14:15:06 +0545

    Delete simulation_engine.py

--- simulation_engine.py
+++
@@ -1,7 +0,0 @@
-# simulation_engine.py
-def simulate_policy(gdp_growth, inflation, stimulus, education_investment):
-    # Calculate simulated outputs
-    sim_gdp = gdp_growth + (stimulus * 0.4) + (education_investment * 0.1)
-    sim_inflation = inflation + (stimulus * 0.2) - (education_investment * 0.05)
-    
-    return sim_gdp, sim_inflation
